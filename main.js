c1d78211f40f512835444ad5c405769bd859843a
DanielWedgbrow <w1611889@student.southstaffs.ac.uk>
2017-04-27 12:11:00 +0100

    .

--- main.js
+++ main.js
@@ -61,6 +61,24 @@ var mainState = {
 	jump: function() {
 		//Add a vertical velocity to the bird
 		this.bird.body.velocity.y = -350;
+        
+        // Slowly rotate the bird downward, up to a certain point
+
+    if (this.bird.angle < 20)
+
+    this.bird.angle += 1;
+        
+        // Create an animation on the bird
+
+        var animation = game.add.tween(this.bird);
+
+        // Change the angle of the bird to -20° in 100 milliseconds
+
+        animation.to({angle: -20}, 100);
+
+        // And start the animation
+
+        animation.start();
 	},
 	
 	//Restart the game
